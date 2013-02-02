d59ed0de6af7e3847b7d04e5a4068d59c87bc932
Thorsten Lorenz <thlorenz@gmx.de>
2013-02-02 00:31:59 -0500

    correctly reporting most sequences

--- parse-key.js
+++ parse-key.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var keycodes = require('./keycodes');
+
 function assertKeyString(s) {
   if (!/^(ctrl-|shift-|alt-|meta-){0,4}\w+$/.test(s))
     throw new Error('The string to parse needs to be of the format "c", "ctrl-c", "shift-ctrl-c".');
@@ -11,17 +13,21 @@ module.exports = function parse(s) {
   assertKeyString(keyString);
 
   var key = {
-      name: undefined 
-    , ctrl: false
-    , meta: false
-    , shift: false
-    , alt: false
+      name     :  undefined
+    , ctrl     :  false
+    , meta     :  false
+    , shift    :  false
+    , alt      :  false
+    , sequence :  undefined
   }
   , parts = keyString.split('-')
   , c;
 
   key.name = parts.pop();
   while((c = parts.pop())) key[c] = true;
+  key.sequence = key.ctrl 
+    ? keycodes.ctrl[key.name] || key.name
+    : keycodes.nomod[key.name] || key.name;
 
   return key;
 };
--- test/parse-key.js
+++ test/parse-key.js
@@ -13,32 +13,32 @@ function assert(t, s, key) {
 }
 
 test('keys without modifiers', function (t) {
-  assert(t, 's', { name: 's', ctrl: false, meta: false, shift: false, alt: false })
-  assert(t, 'c', { name: 'c', ctrl: false, meta: false, shift: false, alt: false })
-  assert(t, 'space', { name: 'space', ctrl: false, meta: false, shift: false, alt: false })
-  assert(t, 'escape', { name: 'escape', ctrl: false, meta: false, shift: false, alt: false })
+  assert(t ,  's'      ,  { name: 's'      ,  ctrl: false ,  meta: false ,  shift: false ,  alt: false ,  sequence: 's'})
+  assert(t ,  'c'      ,  { name: 'c'      ,  ctrl: false ,  meta: false ,  shift: false ,  alt: false ,  sequence: 'c'})
+  assert(t ,  'space'  ,  { name: 'space'  ,  ctrl: false ,  meta: false ,  shift: false ,  alt: false ,  sequence: ' ' })
+  assert(t ,  'escape' ,  { name: 'escape' ,  ctrl: false ,  meta: false ,  shift: false ,  alt: false ,  sequence: '\u001b' })
 })
 
 test('keys with one modifier', function (t) {
-  assert(t, 'ctrl-c'  ,  { name:'c' ,  ctrl: true  ,  meta: false ,  shift: false ,  alt: false })
-  assert(t, 'meta-c'  ,  { name:'c' ,  ctrl: false ,  meta: true  ,  shift: false ,  alt: false })
-  assert(t, 'shift-c' ,  { name:'c' ,  ctrl: false ,  meta: false ,  shift: true  ,  alt: false })
-  assert(t, 'alt-c'   ,  { name:'c' ,  ctrl: false ,  meta: false ,  shift: false ,  alt: true })
+  assert(t, 'ctrl-c'  ,  { name:'c' ,  ctrl: true  ,  meta: false ,  shift: false ,  alt: false, sequence: '\u0003' })
+  assert(t, 'meta-c'  ,  { name:'c' ,  ctrl: false ,  meta: true  ,  shift: false ,  alt: false, sequence: 'c' })
+  assert(t, 'shift-c' ,  { name:'c' ,  ctrl: false ,  meta: false ,  shift: true  ,  alt: false, sequence: 'c' })
+  assert(t, 'alt-c'   ,  { name:'c' ,  ctrl: false ,  meta: false ,  shift: false ,  alt: true, sequence: 'c' })
 
-  assert(t, 'ctrl-space', { name: 'space', ctrl: true, meta: false, shift: false, alt: false })
+  assert(t, 'ctrl-space', { name: 'space', ctrl: true, meta: false, shift: false, alt: false, sequence: '\u0000' })
 })
 
 test('keys with one modifier with mixed casing', function (t) {
-  assert(t, 'Ctrl-c'  ,  { name:'c' ,  ctrl: true  ,  meta: false ,  shift: false ,  alt: false })
-  assert(t, 'meta-C'  ,  { name:'c' ,  ctrl: false ,  meta: true  ,  shift: false ,  alt: false })
-  assert(t, 'Shift-c' ,  { name:'c' ,  ctrl: false ,  meta: false ,  shift: true  ,  alt: false })
-  assert(t, 'ALT-c'   ,  { name:'c' ,  ctrl: false ,  meta: false ,  shift: false ,  alt: true })
+  assert(t ,  'Ctrl-c'  ,  { name:'c' ,  ctrl: true  ,  meta: false ,  shift: false ,  alt: false ,  sequence: '\u0003' })
+  assert(t ,  'meta-C'  ,  { name:'c' ,  ctrl: false ,  meta: true  ,  shift: false ,  alt: false ,  sequence: 'c' })
+  assert(t ,  'Shift-c' ,  { name:'c' ,  ctrl: false ,  meta: false ,  shift: true  ,  alt: false ,  sequence: 'c' })
+  assert(t ,  'ALT-c'   ,  { name:'c' ,  ctrl: false ,  meta: false ,  shift: false ,  alt: true  ,  sequence: 'c' })
 })
 
 test('keys with multiple modifiers', function (t) {
-  assert(t, 'shift-ctrl-c'  ,  { name:'c' ,  ctrl: true  ,  meta: false ,  shift: true ,  alt: false })
-  assert(t, 'alt-meta-c'  ,  { name:'c' ,  ctrl: false ,  meta: true  ,  shift: false ,  alt: true })
-  assert(t, 'shift-ctrl-meta-c' ,  { name:'c' ,  ctrl: true ,  meta: true ,  shift: true  ,  alt: false })
-  assert(t, 'alt-shift-ctrl-meta-c'   ,  { name:'c' ,  ctrl: true ,  meta: true ,  shift: true ,  alt: true })
-  assert(t, 'shift-ctrl-escape', { name: 'escape', ctrl: true, meta: false, shift: true, alt: false })
+  assert(t ,  'shift-ctrl-c'          ,  { name:'c'       ,  ctrl: true  ,  meta: false ,  shift: true  ,  alt: false ,  sequence: '\u0003' })
+  assert(t ,  'alt-meta-c'            ,  { name:'c'       ,  ctrl: false ,  meta: true  ,  shift: false ,  alt: true  ,  sequence: 'c' })
+  assert(t ,  'shift-ctrl-meta-c'     ,  { name:'c'       ,  ctrl: true  ,  meta: true  ,  shift: true  ,  alt: false ,  sequence: '\u0003' })
+  assert(t ,  'alt-shift-ctrl-meta-c' ,  { name:'c'       ,  ctrl: true  ,  meta: true  ,  shift: true  ,  alt: true  ,  sequence: '\u0003' })
+  assert(t ,  'shift-ctrl-escape'     ,  { name: 'escape' ,  ctrl: true  ,  meta: false ,  shift: true  ,  alt: false ,  sequence: 'escape' })
 })
